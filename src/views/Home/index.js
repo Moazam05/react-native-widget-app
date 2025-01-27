30f23c346c897042e8bd4cf4b5c5ab56b07d89e7
Moazam05 <salmanmoazam08@gmail.com>
2025-01-27 16:49:01 +0500

    pkges installed

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,40 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, Button, StyleSheet} from 'react-native';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Button
+        title="In-App Browser"
+        onPress={() => navigation.navigate('Browser')}
+      />
+      <Button
+        title="Recycler View"
+        onPress={() => navigation.navigate('RecyclerView')}
+      />
+      <Button
+        title="Audio Recorder"
+        onPress={() => navigation.navigate('AudioRecorder')}
+      />
+      <Button
+        title="Video Player"
+        onPress={() => navigation.navigate('VideoPlayer')}
+      />
+      <Button
+        title="LLM Integration"
+        onPress={() => navigation.navigate('LLM')}
+      />
+      <Button title="Widgets" onPress={() => navigation.navigate('Widget')} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'space-around',
+  },
+});
 
 export default Home;
