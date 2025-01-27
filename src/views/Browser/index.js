8a3e19f545e12af725fdbd489ddc97bed512b705
Moazam05 <salmanmoazam08@gmail.com>
2025-01-27 16:59:51 +0500

    browser view

--- src/views/Browser/index.js
+++ src/views/Browser/index.js
@@ -1,12 +1,27 @@
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import React from 'react';
+import {WebView} from 'react-native-webview';
 
 const Browser = () => {
   return (
-    <View>
-      <Text>Browser</Text>
+    <View style={styles.container}>
+      <WebView
+        source={{uri: 'https://salman-muazam.netlify.app/'}}
+        style={styles.webview}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
 
 export default Browser;
